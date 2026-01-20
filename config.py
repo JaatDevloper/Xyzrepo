d945ad750b7e5ac61a6507a4468c8fc23c04c20d
.reven <pinterestjat2022@gmail.com>
2026-01-20 12:01:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
+api_id = ""
+api_hash = ""
+bot_token = ""
 import os
-auth_users = [5840594311, int(os.environ.get("USER_ID", 0))]
+auth_users = [7656415064, int(os.environ.get("USER_ID", 0))]
